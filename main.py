b1613499c5f752c7b11902855efc6ae17ab46c38
bouaskaoun <mohammed.bouaskaoun@edu.uca.ma>
2022-06-21 16:57:18 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import Algorithme
+
+
+matrix = Algorithme.loadMatrix()
+if not Algorithme.isSymmetric(matrix):
+	raise ValueError("Not a symmetric matrix.")
+Q, R = Algorithme.QRdecompose(matrix)
+eVectors, eValues = Algorithme.computeVV(Q, R)
+Algorithme.outputResults(eVectors, eValues)
